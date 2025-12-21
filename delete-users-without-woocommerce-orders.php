e540fecbb4a529ae12608ce03b5762cd7c3c6790
Michal Kowalik <klapaucius.mk@gmail.com>
2025-12-21 22:07:54 +0100

    chore: initial commit

---
+++ delete-users-without-woocommerce-orders.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Plugin Name:       Delete Users Without WooCommerce Orders
+ * Plugin URI:        https://wordpress.org/plugins/delete-users-without-woocommerce-orders/
+ * Description:       -
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Michał Kowalik
+ * Author URI:        https://michalkowalik.pl
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       delete-users-without-woocommerce-orders
+ */
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== Delete Users Without WooCommerce Orders ===
+Contributors: klapaucius4
+Tags: delete users, without orders, woocommerce spam
+Requires at least: 5.2
+Tested up to: 6.9
+Stable tag: 1.0.0
+License: GPLv2 or later
+
+The plugin short description...
+
+== Description ==
+To be continued
+
+== Changelog ==
+= 1.0.0 =
+First version
